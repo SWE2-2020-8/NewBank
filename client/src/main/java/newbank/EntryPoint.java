4fa74c5fae6e1f64f4d66eaceda042fd513c8b6f
Gabriel Mesquida-Masana <gabriel.mesquida.e@thalesdigital.io>
2020-07-20 21:34:50 +1000

    Welcome screen

--- client/src/main/java/newbank/EntryPoint.java
+++ client/src/main/java/newbank/EntryPoint.java
@@ -7,17 +7,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+        Parent root = FXMLLoader
+                .load(getClass().getResource("/fxml/Scene.fxml"));
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        
-        stage.setTitle("JavaFX and Maven");
+
+        stage.setTitle("NewBank Client");
         stage.setScene(scene);
         stage.show();
     }
@@ -28,7 +28,8 @@ public class EntryPoint extends Application {
      * launched through deployment artifacts, e.g., in IDEs with limited FX
      * support. NetBeans ignores main().
      *
-     * @param args the command line arguments
+     * @param args
+     *             the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
